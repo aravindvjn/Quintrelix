15aa302ce4ebce197aed9751337927b4fb73489a
aravind <aravind284479@gmail.com>
2024-09-17 13:43:24 +0530

    base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/auth/', // Set the default base path to /myapp/
   plugins: [react()],
-})
+});
