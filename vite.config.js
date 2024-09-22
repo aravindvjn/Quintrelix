6ebd721fc2d0882bff585b0330f72d28ab07c61a
Aravind Vijayan <aravind284479@gmail.com>
2024-09-22 07:43:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  //
-  base: '/auth', // Set the default base path to /myapp/
   plugins: [react()],
+  base: '/', // Ensures routing works correctly in Vercel
 });
